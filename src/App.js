eb954d4c04b77f509bb8b75c07e6a4ec6c5d123f
Jonghun Lee <jonghunjhl@gmail.com>
2020-12-26 10:10:14 -0700

    enable to send data from client to database

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import InputTodo from './components/InputTodo';
+import ListTodos from './components/ListTodos';
 
 function App() {
 	return (
 		<>
 			<div className='container'>
 				<InputTodo />
+				<ListTodos />
 			</div>
 		</>
 	);
--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -2,10 +2,24 @@ import React, { useState } from 'react';
 
 const InputTodo = () => {
 	const [description, setDescription] = useState('');
+	const onSubmitForm = async (e) => {
+		e.preventDefault();
+		try {
+			const body = { description };
+			const response = await fetch('http://localhost:8001/todos', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(body),
+			});
+			window.location = '/';
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
 	return (
 		<>
 			<h1 className='text-center mt-5'>Pern Todo list</h1>
-			<form className='d-flex mt-5'>
+			<form className='d-flex mt-5' onSubmit={onSubmitForm}>
 				<input
 					type='text'
 					className='form-control'
@@ -15,6 +29,7 @@ const InputTodo = () => {
 				/>
 				<button className='btn btn-success'>Add</button>
 			</form>
+			{description}
 		</>
 	);
 };
