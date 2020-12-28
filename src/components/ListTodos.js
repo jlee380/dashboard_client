e9af1ccc16ff60070d2aa4d4361804e31a0cf38f
Jonghun Lee <jonghunjhl@gmail.com>
2020-12-27 19:59:51 -0700

    update has been done

--- src/components/ListTodos.js
+++ src/components/ListTodos.js
@@ -1,36 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ListTodos = () => {
+	const [todos, setTodos] = useState([]);
+	const getTodos = async () => {
+		try {
+			const response = await fetch('http://localhost:8001/todos');
+			const jsonData = await response.json();
+
+			setTodos(jsonData);
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
+	const handleDelete = async (id) => {
+		try {
+			const deleteTodo = await fetch(
+				`http://localhost:8001/todos/${id}`,
+				{
+					method: 'DELETE',
+				}
+			);
+			setTodos(todos.filter((todo) => todo.todo_id !== id));
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
+	useEffect(() => {
+		getTodos();
+	}, []);
+
 	return (
 		<>
-			<table class='table table-striped'>
+			<table class='table table-striped mt-5'>
 				<thead>
 					<tr>
 						<th scope='col'>#</th>
-						<th scope='col'>First</th>
-						<th scope='col'>Last</th>
-						<th scope='col'>Handle</th>
+						<th scope='col'>Description</th>
+						<th scope='col'>Edit</th>
+						<th scope='col'>Delete</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<th scope='row'>1</th>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-					</tr>
-					<tr>
-						<th scope='row'>2</th>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-					</tr>
-					<tr>
-						<th scope='row'>3</th>
-						<td>Larry</td>
-						<td>the Bird</td>
-						<td>@twitter</td>
-					</tr>
+					{todos.map((todo) => (
+						<>
+							<tr>
+								<td scope='row'>1</td>
+								<td key={todo.todo_id}>{todo.description}</td>
+								<td>
+									<button key={todo.todo_id}>edit</button>
+								</td>
+								<td>
+									<button
+										className='btn btn-danger'
+										onClick={() =>
+											handleDelete(todo.todo_id)
+										}>
+										Delete
+									</button>
+								</td>
+							</tr>
+						</>
+					))}
 				</tbody>
 			</table>
 		</>
